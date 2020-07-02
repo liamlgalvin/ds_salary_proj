f61381fcccba0dd7d0b1427e6f48516e50908e32
liamlgalvin <liamlgalvin@gmail.com>
2020-07-02 10:07:24 +0200

    uploading data cleaning script

--- data_cleaning.py
+++ data_cleaning.py
@@ -20,10 +20,11 @@ salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0] )
 minus_kd = salary.apply(lambda x: x.replace('K', '').replace('$',''))
 
 df['employer_provided'] =  df['Salary Estimate'].apply(lambda x: 1 if "employer provided salary:" in x.lower() else 0)
-df['hourly'] =  df['Salary Estimate'].apply(lambda x: 1 if "per hour" in x.lower() else 0)
+df['hourly'] =  df['Salary Estimate'].apply(lambda x: 1 if "por hora" in x.lower() else 0)
 
-df['min_salary'] = minus_kd.apply(lambda x: int(x.split('-')[0].strip()) ) 
-df['max_salary'] = minus_kd.apply(lambda x: int(x.split('-')[1].strip()) ) 
+minus_text = minus_kd.apply(lambda x: x.replace('Por hora', ''))
+df['min_salary'] = minus_text.apply(lambda x: int(x.split('-')[0].strip()) ) 
+df['max_salary'] = minus_text.apply(lambda x: int(x.split('-')[1].strip()) ) 
 df['avg_salry'] = (df.min_salary + df.max_salary)/2
 
 # Company name text only
@@ -33,11 +34,13 @@ df['company_text'] = df.apply(lambda x : x['Company Name'] if x['Rating'] < 0 el
 
 # state field
 
-df['job_state'] = df['Location'].apply(lambda x: x.split(',')[1])
+df['job_state'] = df['Location'].apply(lambda x: x.strip().split(', ')[1] if len(x.strip().split(', ')) > 1 else -1 )
+# remove entries where job state = -1
+df = df[df['job_state'] != -1]
 
 df.job_state.value_counts()
 
-df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquaters else 0, axis = 1)
+df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0, axis = 1)
 
 # age of company 
 
@@ -55,3 +58,8 @@ df['spark_yn'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower()
 df['aws_yn'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0 )
 #excel
 df['excel_yn'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0 )
+
+
+# save csv
+
+df.to_csv("salary_data_cleaned.csv", index = False)
